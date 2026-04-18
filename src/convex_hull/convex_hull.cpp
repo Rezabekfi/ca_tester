ff8f8e788636db58e05013c13422d7eb36d83cc3
Rezabekfi <filip.rezabek@gmail.com>
2026-04-18 10:15:30 +0200

    notes + few key saves

--- src/convex_hull/convex_hull.cpp
+++ src/convex_hull/convex_hull.cpp
@@ -45,11 +45,11 @@ bool ConvexHull::distinct_sets(const RuleContext& ctx, std::size_t nx, std::size
 
 uint8_t ConvexHull::apply(uint8_t current_state, const RuleContext& ctx, const std::vector<uint8_t>& neighbours) const {
   // TODO: remove the magic number 20 later and make it configurable or change the logic to check if there was no change in last iterations
-  if (is_seed(current_state) || (is_marked(current_state) && !(ctx.getIteration() % 20 == 0)))  {
+  if (is_seed(current_state) || (is_marked(current_state) && !(ctx.getIteration() % ctx.getGrid().getWidth() == 0)))  {
     return current_state; // seeds remain unchanged
   }
 
-  if (ctx.getIteration() % 20 == 0 && ctx.getIteration() != 0) {
+  if (ctx.getIteration() % ctx.getGrid().getWidth() == 0 && ctx.getIteration() != 0) {
     if (is_marked(current_state)) {
       return create_cell(true, false, 0); // making seeds from marked cells every 20 iterations this will be changed to check if there was no change in last iterations later
     }
